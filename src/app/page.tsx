bc7b214a3b6f687af4952068c4068f3bf3f97fc1
cup1111 <5finoilheater@gmail.com>
2025-07-26 16:34:53 +1000

    auth deployment debug

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
-import Link from "next/link";
+//import Link from "next/link";
 
-import { LatestPost } from "~/app/_components/post";
+//import { LatestPost } from "~/app/_components/post";
 import { api, HydrateClient } from "~/trpc/server";
 import { AuthForm } from "~/app/_components/authform";
 
 export default async function Home() {
-  const hello = await api.post.hello({ text: "from tRPC" });
+//  const hello = await api.post.hello({ text: "from tRPC" });
 
   void api.post.getLatest.prefetch();
 
